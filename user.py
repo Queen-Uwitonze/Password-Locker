1e1dfa621217908e7f3089accdaa2648be8792d6
Queen-Uwitonze <uwitonzeq@gmail.com>
2019-02-12 09:22:29 +0200

    first error to test user class

--- user.py
+++ user.py
@@ -2,8 +2,6 @@ class User:
     """
     Class that generates new instances of users.
     """
-
-    users_list = [] # Empty contact list
     # Init method up here
     def __init__(self,user_name,email,password):
       self.user_name = user_name
