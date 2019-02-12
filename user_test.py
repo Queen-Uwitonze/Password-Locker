bd5f472577f1a02fad5c3ca6a35608efcea66d57
Queen-Uwitonze <uwitonzeq@gmail.com>
2019-02-12 09:38:53 +0200

    second testing error for saving my user list

--- user_test.py
+++ user_test.py
@@ -24,5 +24,13 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_person.user_email,"uwitonzeq@gmail.com")
         self.assertEqual(self.new_person.user_password,"wito123")
 
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+         the user list
+        '''
+        self.new_person.save_user() # saving the new user
+        self.assertEqual(len(User.user_list),1)
+
 if __name__ == '__main__':
     unittest.main()
