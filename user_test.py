5dba286f1e1a15a815138652f153b227fb652789
Queen-Uwitonze <uwitonzeq@gmail.com>
2019-02-12 10:42:20 +0200

    correcting the third error for saving multiples users

--- user_test.py
+++ user_test.py
@@ -3,44 +3,52 @@ from user import User # Importing the contact class
 
 class TestUser(unittest.TestCase):
 
+  '''
+  Test class that defines test cases for the user class behaviours.
+
+  Args:
+      unittest.TestCase: TestCase class that helps in creating test cases
+  '''
+  def setUp(self):
+      '''
+      Set up method to run before each test cases.
+      '''
+      self.new_person= User("Queen-Uwitonze","uwitonzeq@gmail.com","wito123") # create user object
+
+  def test_init(self):
+      '''
+      test_init test case to test if the object is initialized properly
+      '''
+
+      self.assertEqual(self.new_person.user_name,"Queen-Uwitonze")
+      self.assertEqual(self.new_person.user_email,"uwitonzeq@gmail.com")
+      self.assertEqual(self.new_person.user_password,"wito123")
+
+  def test_save_user(self):
+      '''
+      test_save_user test case to test if the user object is saved into
+        the user list
+      '''
+      self.new_person.save_user() # saving the new user
+      self.assertEqual(len(User.user_list),1)
+
+  def tearDown(self):
+
     '''
-    Test class that defines test cases for the user class behaviours.
-
-    Args:
-        unittest.TestCase: TestCase class that helps in creating test cases
+    tearDown method that does clean up after each test case has run.
     '''
-    def setUp(self):
-        '''
-        Set up method to run before each test cases.
-        '''
-        self.new_person= User("Queen-Uwitonze","uwitonzeq@gmail.com","wito123") # create user object
+    User.user_list = []
 
-    def test_init(self):
-        '''
-        test_init test case to test if the object is initialized properly
-        '''
+  def test_save_multiple_user_contact(self):
 
-        self.assertEqual(self.new_person.user_name,"Queen-Uwitonze")
-        self.assertEqual(self.new_person.user_email,"uwitonzeq@gmail.com")
-        self.assertEqual(self.new_person.user_password,"wito123")
-
-    def test_save_user(self):
-        '''
-        test_save_user test case to test if the user object is saved into
-         the user list
-        '''
-        self.new_person.save_user() # saving the new user
-        self.assertEqual(len(User.user_list),2)
-
-    def test_save_multiple_user_contact(self):
-            '''
-            test_save_multiple_user-contact to check if we can save multiple user
-            objects to our user_list
-            '''
-            self.new_person.save_user()
-            test_person = User("Test","user","test@user.com") # new user
-            test_person.save_user()
-            self.assertEqual(len(User.user_list),2)
+    '''
+    test_save_multiple_user-contact to check if we can save multiple user
+    objects to our user_list
+    '''
+    self.new_person.save_user()
+    test_person = User("Test","test@user.com","muraho") # new user
+    test_person.save_user()
+    self.assertEqual(len(User.user_list),2)
 
 if __name__ == '__main__':
     unittest.main()
